1ea8848ab47956d70df3f3cf59c4a36bfe8fc646
flypat02 <100413785+flypat02@users.noreply.github.com>
2025-03-31 12:57:13 +0900

    Time: 0 ms (100%), Space: 19.5 MB (19.85%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        keys = set(nums)  
+        answer = 0
+        
+        for key in keys:  
+            if nums.count(key) > len(nums) / 2:  
+                answer = key
+                break
+        return answer
